5bf2d90795c9f0c0f575981c550fd96f9364d6ff
emilyambros <emily25ambros@gmail.com>
2023-12-17 23:16:52 +0200

    commit

--- 12. tund/router_server/server.js
+++ 12. tund/router_server/server.js
@@ -5,21 +5,28 @@ const path = require('path');
 function serveFile(path, contentType, res) {
     fs.readFile(path, (err, data) => {
         if (err) {
-            res.writeHead(500, {'Content-type': 'text/plain'});
+            res.writeHead(500, {'Content-Type': 'text/plain'});
             return res.end('Error faili laadimisega');
         }
-        res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(data)
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(data);
     })
 }
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        serveFile('index.html', 'text/htnl', res);
+    if (req.url === '/') {
+        serveFile('index.html', 'text/html', res);
     } else if (req.url === '/about') {
         serveFile('about.html', 'text/html', res);
-    } else if (req.url.match('')) {
+    } else if (req.url.match('\.css$')) {
         var cssPath = path.join(__dirname, req.url);
-        serveFile(cssP)
+        serveFile(cssPath, 'text/css', res);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Lehte ei leitud');
     }
+})
+
+server.listen(3001, () => {
+    console.log('Server kuulab pordil 3001. Külasta http://localhost:3001')
 })
